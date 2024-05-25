557c18dff5b4b2fcabff04b408256c0e7c114c47
David Brett <davidbrett88@gmail.com>
2024-05-25 19:45:24 +0100

    add prompt to select starting station

--- zone1tube/main.py
+++ zone1tube/main.py
@@ -1,4 +1,5 @@
 from logging import INFO, basicConfig, log
+from typing import Annotated
 
 import typer
 
@@ -9,10 +10,17 @@ typer_handler = TyperLoggerHandler()
 basicConfig(level=INFO, handlers=(typer_handler,))
 
 
-def main():
+def main(starting_station: Annotated[str, typer.Option(help="Starting station, must be in Zone 1.", prompt=True)]):
     with open("./data.json", "r") as f:
         stations = Stations.validate_json(f.read())
     log(level=INFO, msg=f"Loaded {len(stations)} stations.")
+
+    # stations in zone 1
+    zone1_stations = [s for s in stations if s.zone == 1.0]
+    log(level=INFO, msg=f"Found {len(zone1_stations)} zone 1 stations.")
+    if not any(s.name == starting_station for s in zone1_stations):
+        raise ValueError("starting_station not in zone 1")
+    log(level=INFO, msg=f"Starting station set as '{starting_station}'.")
 
 
 if __name__ == "__main__":
